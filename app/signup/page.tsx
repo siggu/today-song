2cc1601b2d20d0ef4a9ac84005b5d38b5cd1e9f3
siggu <siggu100@gmail.com>
2025-07-30 22:57:16 +0900

    feat: 로그인, 로그아웃 마크업 변경

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -8,32 +8,34 @@ export default function Signup() {
   const [isLogin, setIsLogin] = useState(false);
 
   return (
-    <main className='font-jua'>
-      <header className='bg-white max-w-[1440px] mx-auto py-10 px-10'>
-        <div className='flex justify-between items-center'>
-          <div className='text-4xl font-bold'>오늘의 노래</div>
-          <div className='flex items-center gap-2 w-[60%]'>
-            <input
-              type='text'
-              placeholder='노래 검색'
-              className='w-full border-2 border-gray-300 rounded-full px-4 py-2 focus:outline-none'
-            />
-            <button className='bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600'>
-              <MagnifyingGlassIcon className='w-6 h-6' />
-            </button>
-          </div>
-          <div className='flex items-center gap-2'>
-            {isLogin ? (
-              <button className='bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600'>
-                <UserIcon className='w-6 h-6' />
+    <main className='font-jua bg-purple-50 min-h-screen'>
+      <header className='bg-white w-full py-5 px-6'>
+        <div className='max-w-[1440px] mx-auto'>
+          <div className='flex justify-between items-center'>
+            <div className='text-4xl font-bold'>오늘의 노래</div>
+            <div className='flex items-center gap-2 w-[60%]'>
+              <input
+                type='text'
+                placeholder='노래 검색'
+                className='w-full border-2 border-gray-300 rounded-full px-4 py-2 focus:outline-none'
+              />
+              <button className='bg-purple-500 text-white rounded-full px-4 py-2 hover:bg-purple-600'>
+                <MagnifyingGlassIcon className='w-6 h-6' />
               </button>
-            ) : (
-              <button className='bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600'>로그인</button>
-            )}
+            </div>
+            <div className='flex items-center gap-2'>
+              {isLogin ? (
+                <button className='bg-purple-500 text-white rounded-full px-4 py-2 hover:bg-purple-600'>
+                  <UserIcon className='w-6 h-6' />
+                </button>
+              ) : (
+                <button className='bg-purple-500 text-white rounded-full px-4 py-2 hover:bg-purple-600'>로그인</button>
+              )}
+            </div>
           </div>
         </div>
       </header>
-      <section className='bg-white max-w-[1440px] mx-auto h-[60vh] flex flex-col items-center justify-center gap-6'>
+      <section className='max-w-[1440px] mx-auto h-[60vh] flex flex-col items-center justify-center gap-6'>
         <div className='flex flex-col items-center justify-center gap-10'>
           <div className='text-4xl font-bold'>회원가입</div>
           <div className='flex flex-col items-center justify-center gap-2'>
@@ -60,12 +62,9 @@ export default function Signup() {
           </div>
         </div>
         <div className='flex items-center justify-center gap-3 w-full max-w-[500px]'>
-          <Link
-            href='/login'
-            className='bg-black text-white text-center rounded-full px-4 py-2 hover:bg-black/80  w-full hover:cursor-pointer'
-          >
-            <button>회원가입</button>
-          </Link>
+          <button className='bg-purple-500 text-white rounded-full px-4 py-2 hover:bg-purple-600 w-full hover:cursor-pointer'>
+            회원가입
+          </button>
         </div>
       </section>
     </main>
